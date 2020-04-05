78b5eb78fec63bfcb8ded33ac7cbdc3d6f4c884c
embar- <embar-@users.noreply.github.com>
2020-04-05 20:02:38 +0300

    Update lt.js
    Fix misspelled "rugsėjis", change weekdays to usually usable format, some other minor changes

--- media/system/js/fields/calendar-locales/lt.js
+++ media/system/js/fields/calendar-locales/lt.js
@@ -4,9 +4,9 @@ window.JoomlaCalLocale = {
 	wk : "wk",
 	time : "Laikas:",
 	days : ["Sekmadienis", "Pirmadienis", "Antradienis", "Trečiadienis", "Ketvirtadienis", "Penktadienis", "Šeštadienis"],
-	shortDays : ["Sek", "Pir", "Ant", "Tre", "Ket", "Pen", "Šeš"],
-	months : ["Sausis", "Vasaris", "Kovas", "Balandis", "Gegužė", "Birželis", "Liepa", "Rugpjūtis", "Rugsšjis", "Spalis", "Lapkritis", "Gruodis"],
-	shortMonths : ["Sau", "Vas", "Kov", "Bal", "Geg", "Bir", "Lie", "Rug", "Rgs", "Spa", "Lap", "Grd"],
+	shortDays : ["Sk", "Pr", "An", "Tr", "Kt", "Pn", "Šš"],
+	months : ["Sausis", "Vasaris", "Kovas", "Balandis", "Gegužė", "Birželis", "Liepa", "Rugpjūtis", "Rugsėjis", "Spalis", "Lapkritis", "Gruodis"],
+	shortMonths : ["Sau", "Vas", "Kov", "Bal", "Geg", "Bir", "Lie", "Rgp", "Rgs", "Spa", "Lap", "Grd"],
 	AM : "AM",
 	PM :  "PM",
 	am : "am",
@@ -14,6 +14,6 @@ window.JoomlaCalLocale = {
 	dateType : "gregorian",
 	minYear : 1900,
 	maxYear : 2100,
-	exit: "Uždaryti",
+	exit: "Užverti",
 	clear: "Išvalyti"
 };
